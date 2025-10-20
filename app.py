0047d03df6cde919120c2bca4561dc80b67aabc5
Leticia <leticiasantana0607@gmail.com>
2025-10-19 21:49:47 -0300

    refatorando

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza Hut', 'McDonalds', 'Burger King']
+
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -11,43 +13,63 @@ def exibir_nome_do_programa():
 """)
 
 def exibir_opcoes():
-    print('1. Cadastrar em corridas')
-    print('2. Listar melhores corridas')
-    print('3. Atividades em São Paulo')
+    print('1. Cadastrar restaurantes')
+    print('2. Listar restaurantes')
+    print('3. Reativar cadastro')
     print('4. Sair\n')
 
 def finalizar_app():
+    exibir_subtitulo('Finalizando app!')
+
+def voltar_ao_menu_principal():
+    input('\nPressione Enter para voltar ao menu principal...')
+    main() 
+    
+def opcao_invalida():
+    print('Opção inválida! Tente novamente.\n')
+    voltar_ao_menu_principal()    
+
+def exibir_subtitulo(texto):
     os.system('cls')
-    # os.system('clear')
-    print('Finalizando o app')
+    print(texto)
+    print('\n')
+
+def cadastrar_novo_restaurante():
+    ## Limpa a tela e exibe subtítulo
+    exibir_subtitulo('Cadastrar novo restaurante')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    print(f'O restaurante: {nome_do_restaurante} foi cadastrada com sucesso!')
+    
+    ## Adicione o nome da corrida à lista de corridas
+    restaurantes.append(nome_do_restaurante)
+    voltar_ao_menu_principal()    
+
+    
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+        
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+        
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    # opcao_escolhida = int(opcao_escolhida)
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+        
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()     
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            reativar_cadastro()
+        else:
+            finalizar_app()
+    except ValueError:
+        opcao_invalida()        
+     
     
-    if opcao_escolhida == 1:
-        cadastrar_corrida()     
-    elif opcao_escolhida == 2:
-        listar_corridas()
-    elif opcao_escolhida == 3:
-        reativar_cadastro()
-    else:
-        finalizar_app()
-
-def cadastrar_corrida():
-    print('Cadastrar em Corridas')
-    nome = input('Digite seu nome: ')
-    email = input('Digite seu email: ')
-    idade = int(input('Digite sua idade: '))
-    print(f'Nome: {nome}\nEmail: {email}\nIdade: {idade}')
-    print('Cadastro realizado com sucesso!')   
-
-def listar_corridas():
-    print('Lista das corridas que você pode participar:')
-    print('1. São Silvestre - 15km')
-    print('2. Meia Maratona de São Paulo - 21km')
-    print('3. Maratona de São Paulo - 42km')
-    print('4. Volta da Pampulha - 18km')
 
 def reativar_cadastro():  
     print('Reativar cadastro')  
